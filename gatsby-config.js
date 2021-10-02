2b01bf165ad91fbc9c6859770ba96d73706b4659
Jananadi Wedagedara <jananadi92@gmail.com>
2021-10-02 17:50:40 +0900

    Change site title name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
+    title: "Daily Dev Log",
   },
   plugins: ["gatsby-plugin-sass"],
 };

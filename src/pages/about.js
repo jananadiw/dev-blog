f301f52794761f51e8caea6cfe6093707cdefa6b
Jananadi Wedagedara <jananadi92@gmail.com>
2021-10-05 02:46:13 +0900

    Complete Gatsby Tut - part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const aboutPage = () => {
+    return (
+        <Layout pageTitle="About me">
+            <p>Hi there! 🙌🏼</p>
+        </Layout>
+    )
+}
+
+export default aboutPage;
